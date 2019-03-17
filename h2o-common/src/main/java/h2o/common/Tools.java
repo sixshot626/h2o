f0175e62ae8b0f9cd122b3519169f10a60e2567c
sixshot626 <sixshot626@gmail.com>
2019-03-17 20:02:55 +0800

    清理

--- h2o-common/src/main/java/h2o/common/Tools.java
+++ h2o-common/src/main/java/h2o/common/Tools.java
@@ -8,14 +8,8 @@ import h2o.common.util.bean.support.MapVOImpl;
 
 public class Tools {
 
-
 	public static final BeanUtil b = new BeanUtil(new JoddBeanUtilVOImpl(true,false) , null);
 	
-	public static final BeanUtil bu = new BeanUtil(new JoddBeanUtilVOImpl(true,false) ,
-			new MapVOImpl( new CasePreOperateImpl("Upper")));
-
 	public static final Logger log = new Logger();
-	
 
-	
 }
