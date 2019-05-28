f7b936d8a2aaf2102c4ee1a48dbec78a8f1ba57f
张建伟 <zhangjw626@qq.com>
2019-05-28 14:19:17 +0800

    改用workerId

--- h2o-common/src/main/java/h2o/common/util/id/SnowflakeIdGen.java
+++ h2o-common/src/main/java/h2o/common/util/id/SnowflakeIdGen.java
@@ -7,7 +7,7 @@ package h2o.common.util.id;
  * 1位标识，由于long基本类型在Java中是带符号的，最高位是符号位，正数是0，负数是1，所以id一般是正数，最高位是0<br>
  * 41位时间截(毫秒级)，注意，41位时间截不是存储当前时间的时间截，而是存储时间截的差值（当前时间截 - 开始时间截)
  * 得到的值），这里的的开始时间截，一般是我们的id生成器开始使用的时间，由我们程序来指定的（如下下面程序IdWorker类的startTime属性）。41位的时间截，可以使用69年，年T = (1L << 41) / (1000L * 60 * 60 * 24 * 365) = 69<br>
- * 10位的数据机器位，可以部署在1024个节点，包括7位datacenterId和3位timerId<br>
+ * 10位的数据机器位，可以部署在1024个节点，包括3位timerId和7位workerId<br>
  * 12位序列，毫秒内的计数，12位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生4096个ID序号<br>
  * 加起来刚好64位，为一个Long型。<br>
  * SnowFlake的优点是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，并且效率较高，经测试，SnowFlake每秒能够产生26万ID左右。
@@ -21,44 +21,44 @@ public class SnowflakeIdGen {
     private final long twepoch = 1500000000000L;
 
     /**
-     * 时钟id所占的位数
+     * 机器ID所占的位数
+     */
+    private final long workerIdBits = 7L;
+
+    /**
+     * 时钟ID所占的位数
      */
     private final long timerIdBits = 3L;
 
     /**
-     * 数据标识id所占的位数
+     * 支持的最大机器ID，结果是31 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数)
      */
-    private final long datacenterIdBits = 7L;
-
-    /**
-     * 支持的最大机器id，结果是31 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数)
-     */
-    private final long maxTimerId = -1L ^ (-1L << timerIdBits);
+    private final long maxWorkerId = -1L ^ (-1L << workerIdBits);
 
     /**
      * 支持的最大数据标识id，结果是31
      */
-    private final long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
+    private final long maxTimerId = -1L ^ (-1L << timerIdBits);
 
     /**
-     * 序列在id中占的位数
+     * 序列在ID中占的位数
      */
     private final long sequenceBits = 12L;
 
     /**
      * 机器ID向左移12位
      */
-    private final long timerIdShift = sequenceBits;
+    private final long workerIdShift = sequenceBits;
 
     /**
-     * 数据标识id向左移17位(12+5)
+     * 时钟ID向左移19位(12+7)
      */
-    private final long datacenterIdShift = sequenceBits + timerIdBits;
+    private final long timerIdShift = sequenceBits + workerIdBits;
 
     /**
-     * 时间截向左移22位(5+5+12)
+     * 时间截向左移22位(12+7+3)
      */
-    private final long timestampLeftShift = sequenceBits + timerIdBits + datacenterIdBits;
+    private final long timestampLeftShift = sequenceBits + workerIdBits + timerIdBits;
 
     /**
      * 生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095)
@@ -66,14 +66,19 @@ public class SnowflakeIdGen {
     private final long sequenceMask = -1L ^ (-1L << sequenceBits);
 
     /**
+     * 时钟ID的掩码
+     */
+    private final long timeMask = -1L ^ (-1L << timerIdBits);
+
+    /**
+     * 工作机器ID(0~127)
+     */
+    private long workerId;
+
+    /**
      * 时钟ID(0~7)
      */
     private long timerId;
-
-    /**
-     * 数据标识ID(0~127)
-     */
-    private long datacenterId;
 
     /**
      * 毫秒内序列(0~4095)
@@ -88,24 +93,25 @@ public class SnowflakeIdGen {
     //==============================Constructors=====================================
 
 
-    public SnowflakeIdGen( long datacenterId ) {
-        this( datacenterId , 0L );
+    public SnowflakeIdGen(long workerId) {
+        this( workerId , 0L );
     }
+
 
     /**
      * 构造函数
-     * @param datacenterId 数据标识ID (0~127)
-     * @param timerId 时钟id (0~7)
+     *
+     * @param workerId     工作ID (0~127)
+     * @param timerId      时钟ID (0~7)
      */
-    public SnowflakeIdGen( long datacenterId , long timerId ) {
-
-        if ( timerId > maxTimerId || timerId < 0) {
+    public SnowflakeIdGen(long workerId, long timerId) {
+        if (workerId > maxWorkerId || workerId < 0) {
+            throw new IllegalArgumentException(String.format("worker Id can't be greater than %d or less than 0", maxWorkerId));
+        }
+        if (this.timerId > maxTimerId || this.timerId < 0) {
             throw new IllegalArgumentException(String.format("timer Id can't be greater than %d or less than 0", maxTimerId));
         }
-        if (datacenterId > maxDatacenterId || datacenterId < 0) {
-            throw new IllegalArgumentException(String.format("datacenter Id can't be greater than %d or less than 0", maxDatacenterId));
-        }
-        this.datacenterId = datacenterId;
+        this.workerId = workerId;
         this.timerId = timerId;
     }
 
@@ -117,14 +123,12 @@ public class SnowflakeIdGen {
      * @return SnowflakeId
      */
     public synchronized long nextId() {
+
         long timestamp = timeGen();
 
         //如果当前时间小于上一次ID生成的时间戳，说明系统时钟回退过
         if (timestamp < lastTimestamp) {
-            timerId ++;
-            if ( timerId > maxTimerId ) {
-                timerId = 0;
-            }
+            timerId = (timerId + 1) & timeMask;
         }
 
         //如果是同一时间生成的，则进行毫秒内序列
@@ -146,8 +150,8 @@ public class SnowflakeIdGen {
 
         //移位并通过或运算拼到一起组成64位的ID
         return ((timestamp - twepoch) << timestampLeftShift) //
-                | (datacenterId << datacenterIdShift) //
                 | (timerId << timerIdShift) //
+                | (workerId << workerIdShift) //
                 | sequence;
     }
 
@@ -175,13 +179,19 @@ public class SnowflakeIdGen {
     }
 
 
+    protected long getWorkerId() {
+        return workerId;
+    }
 
+    protected void setWorkerId(long workerId) {
+        this.workerId = workerId;
+    }
 
-    public long getTimerId() {
+    protected long getTimerId() {
         return timerId;
     }
 
-    public long getDatacenterId() {
-        return datacenterId;
+    protected void setTimerId(long timerId) {
+        this.timerId = timerId;
     }
 }
