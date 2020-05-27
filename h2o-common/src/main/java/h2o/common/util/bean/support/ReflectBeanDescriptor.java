9e88d38a33afd152a9c7dab9149182e7e8ad742a
张建伟 <zhangjw626@qq.com>
2020-05-27 18:52:08 +0800

    通过反射分析bean属性

---
+++ h2o-common/src/main/java/h2o/common/util/bean/support/ReflectBeanDescriptor.java
@@ -0,0 +1,37 @@
+package h2o.common.util.bean.support;
+
+import h2o.common.collections.builder.ListBuilder;
+import h2o.common.util.bean.BeanDescriptor;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.List;
+
+public class ReflectBeanDescriptor implements BeanDescriptor {
+
+    @Override
+    public String[] getPrepNames(Object bean) {
+
+        return bean instanceof Class ?
+                getClassFieldNames((Class)bean) : getClassFieldNames(bean.getClass());
+
+
+    }
+
+
+    public static String[] getClassFieldNames( Class clazz ) {
+
+        Field[] fields = clazz.getDeclaredFields();
+
+        List<String> fieldNameList = ListBuilder.newList();
+        for ( Field field : fields ) {
+            if ( ! Modifier.isStrict( field.getModifiers() ) ) {
+                fieldNameList.add( field.getName() );
+            }
+        }
+
+        return fieldNameList.toArray( new String[0] );
+    }
+
+
+}
