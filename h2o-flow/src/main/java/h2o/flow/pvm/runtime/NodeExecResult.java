fbe3d17071f7a6f198edb6cb676b1747fafd6ecf
张建伟 <zhangjw626@qq.com>
2020-07-30 14:06:22 +0800

    节点运行结果

--- h2o-flow/src/main/java/h2o/flow/pvm/runtime/NodeExecResult.java
+++ h2o-flow/src/main/java/h2o/flow/pvm/runtime/NodeExecResult.java
@@ -1,8 +1,9 @@
 package h2o.flow.pvm.runtime;
 
+import h2o.common.collections.builder.ListBuilder;
 import h2o.flow.pvm.elements.Line;
-import h2o.flow.pvm.runtime.RunStatus;
 
+import java.util.Collections;
 import java.util.List;
 
 public class NodeExecResult implements java.io.Serializable {
@@ -11,9 +12,14 @@ public class NodeExecResult implements java.io.Serializable {
 
     private final List<Line> lines;
 
-    public NodeExecResult(RunStatus status, List<Line> lines) {
+    public NodeExecResult( RunStatus status, Line line ) {
         this.status = status;
-        this.lines = lines;
+        this.lines = Collections.unmodifiableList(ListBuilder.newList(line));
+    }
+
+    public NodeExecResult( RunStatus status, List<Line> lines ) {
+        this.status = status;
+        this.lines = Collections.unmodifiableList(lines);
     }
 
     public RunStatus getStatus() {
