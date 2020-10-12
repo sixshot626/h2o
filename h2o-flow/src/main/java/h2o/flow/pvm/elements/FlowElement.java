1604e0515abc45b92bb19c5791f9cb692315df15
张建伟 <zhangjw626@qq.com>
2020-10-12 13:11:06 +0800

    调整设计

--- h2o-flow/src/main/java/h2o/flow/pvm/elements/FlowElement.java
+++ h2o-flow/src/main/java/h2o/flow/pvm/elements/FlowElement.java
@@ -3,16 +3,5 @@
  */
 package h2o.flow.pvm.elements;
 
-import java.util.Map;
-
 public interface FlowElement {
-	
-	Object getId();
-	
-	String getName();
-	
-	Object getProperty(String name);
-	
-	Map<String,Object> getProperties();
-
 }
