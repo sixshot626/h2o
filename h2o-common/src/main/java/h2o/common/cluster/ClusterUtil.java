e5c95005aa71675633034113cb9303fc6afd04a1
张建伟 <zhangjw626@qq.com>
2020-11-06 18:35:09 +0800

    优化

--- h2o-common/src/main/java/h2o/common/cluster/ClusterUtil.java
+++ h2o-common/src/main/java/h2o/common/cluster/ClusterUtil.java
@@ -3,12 +3,11 @@ package h2o.common.cluster;
 
 import h2o.common.Tools;
 import h2o.common.lang.SDate;
-import h2o.common.util.date.DateUtil;
 import h2o.common.util.id.SnowGarlandIdGen;
-import h2o.common.util.lang.StringUtil;
 import org.apache.commons.lang.StringUtils;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
 
 public abstract class ClusterUtil {
 
@@ -28,16 +27,13 @@ public abstract class ClusterUtil {
         private final SnowGarlandIdGen idGen = new SnowGarlandIdGen( ClusterUtil.getWorkerId() );
 
         public synchronized String makeId() {
-            Date cd = new Date();
-            String yyyy = DateUtil.toString(cd , "yyyy");
-            return StringUtil.build(
-                        StringUtils.substring( yyyy , -2 )  ,
-                        idGen.nextKey( new SDate( StringUtil.build( yyyy , "-01-01" ) ,true )  )
-                    );
+            LocalDate localDate = LocalDate.now( ZoneOffset.UTC );
+            String yyyy = Integer.toString(localDate.getYear());
+            return StringUtils.substring( yyyy , -2 ) +
+                    idGen.nextKey( new SDate( yyyy  + "-01-01"  ,true )  );
         }
 
     }
-
 
 
 }
