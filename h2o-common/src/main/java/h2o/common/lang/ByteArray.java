e6e8b6f325957d78e832f06b0086d62cfaf05d50
张建伟 <zhangjw626@qq.com>
2021-03-03 16:36:38 +0800

    ByteArray

--- h2o-common/src/main/java/h2o/common/lang/ByteArray.java
+++ h2o-common/src/main/java/h2o/common/lang/ByteArray.java
@@ -5,45 +5,13 @@ public class ByteArray implements NullableValue , java.io.Serializable {
     private static final long serialVersionUID = -5075649510738424502L;
 
     private final byte[] value;
-    private final int offset;
-    private final int size;
-
 
     public ByteArray() {
         this.value = null;
-        this.offset = 0;
-        this.size = 0;
     }
 
     public ByteArray( byte[] value ) {
         this.value = value;
-        this.offset = 0;
-        this.size = value == null ? 0 : value.length;
-    }
-
-    public ByteArray( byte[] value , int size ) {
-        this.value = value;
-        this.offset = 0;
-        this.size = size;
-    }
-
-    public ByteArray( byte[] value , int offset , int size ) {
-        this.value = value;
-        this.offset = offset;
-        this.size = size;
-    }
-
-    public ByteArray( byte[] src , int offset , int size , boolean copy ) {
-        if ( copy ) {
-            this.value = new byte[size];
-            this.offset = 0;
-            this.size = size;
-            System.arraycopy(src, offset, this.value, 0, size);
-        } else {
-            this.value = src;
-            this.offset = offset;
-            this.size = size;
-        }
     }
 
     @Override
@@ -65,24 +33,12 @@ public class ByteArray implements NullableValue , java.io.Serializable {
 
     }
 
-    public int offset() {
-        return offset;
-    }
-
-    public int size() {
-        return size;
-    }
-
 
     @Override
     public String toString() {
 
         if ( this.isPresent() ) {
-            if ( offset == 0 ) {
-                return "byte[" + offset + ".." + size + "]";
-            } else {
-                return "byte[" + size + "]";
-            }
+            return "byte[" + this.value.length + "]";
         } else {
             return "<null>";
         }
