0131a2fb55f95bfd84b47a73a99f09dcac0a3c25
张建伟 <zhangjw626@qq.com>
2021-03-14 23:56:13 +0800

    清理

--- h2o-common/src/main/java/h2o/common/thirdparty/smooks/ExecutionContextSetCallback.java
+++
@@ -1,9 +0,0 @@
-package h2o.common.thirdparty.smooks;
-
-import org.milyn.container.ExecutionContext;
-
-public interface ExecutionContextSetCallback {
-	
-	void setExecutionContext(ExecutionContext executionContext);
-
-}
