ae1d1abef2a1fff4ac9286cd0fc2492a0413c51f
zhangjw <zhangjw626@qq.com>
2022-09-02 11:57:49 +0800

    v6.1.0

--- h2o-flow/src/main/java/h2o/flow/pvm/runtime/RuntimeScopeObject.java
+++ h2o-flow/src/main/java/h2o/flow/pvm/runtime/RuntimeScopeObject.java
@@ -1,12 +1,99 @@
 package h2o.flow.pvm.runtime;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class RuntimeScopeObject implements java.io.Serializable {
 
     private static final long serialVersionUID = -4518026306322958517L;
 
-    public final Map<String,Object> data = new HashMap<>();
+    private Map<String,Object> data;
 
+    public Object get(Object key) {
+
+        if ( data == null ) {
+            return null;
+        }
+
+        return data.get(key);
+    }
+
+    public Object put(String key, Object value) {
+
+        if ( data == null ) {
+            this.data = new HashMap<>();
+        }
+
+        return data.put(key, value);
+    }
+
+    public Object remove(Object key) {
+
+        if ( data == null ) {
+            return null;
+        }
+
+        return data.remove(key);
+    }
+
+    public void putAll(Map<? extends String, ?> m) {
+
+        if ( data == null ) {
+            this.data = new HashMap<>();
+        }
+
+        data.putAll(m);
+    }
+
+    public void clear() {
+
+        if ( this.data != null ) {
+            data.clear();
+        }
+
+    }
+
+    public Set<String> keySet() {
+
+        if ( this.data == null ) {
+            return Collections.EMPTY_SET;
+        }
+
+        return data.keySet();
+    }
+
+    public Collection<Object> values() {
+
+        if ( this.data == null ) {
+            return Collections.EMPTY_LIST;
+        }
+
+        return data.values();
+    }
+
+    public Set<Map.Entry<String, Object>> entrySet() {
+
+        if ( this.data == null ) {
+            return Collections.EMPTY_SET;
+        }
+
+        return data.entrySet();
+    }
+
+    public Object getOrDefault(Object key, Object defaultValue) {
+
+        if ( this.data == null ) {
+            return defaultValue;
+        }
+
+        return data.getOrDefault(key, defaultValue);
+    }
+
+    public Map<String,Object> toMap() {
+
+        if ( this.data == null ) {
+            return Collections.EMPTY_MAP;
+        }
+
+        return Collections.unmodifiableMap( this.data );
+    }
 }
