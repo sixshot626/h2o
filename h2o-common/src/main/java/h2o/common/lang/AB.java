b239c1fe3f91ff51a72535cd2b318b1e1296c7d8
zhangjw <zhangjw626@qq.com>
2022-11-26 15:38:02 +0800

    AB

---
+++ h2o-common/src/main/java/h2o/common/lang/AB.java
@@ -0,0 +1,46 @@
+package h2o.common.lang;
+
+public class AB<X,Y> implements java.io.Serializable {
+
+    private static final long serialVersionUID = 7593389607204994788L;
+
+    public final boolean ok;
+    public final NString msg;
+    public final Val<X> a;
+    public final Val<Y> b;
+
+    public AB( boolean ok, NString msg, Val<X> a, Val<Y> b ) {
+        this.ok = ok;
+        this.msg = msg;
+        this.a = a;
+        this.b = b;
+    }
+
+    public static <R,S>  AB<R,S> a( Val<R> a ) {
+        return new AB<>(true , NString.NULL , a , Val.empty() );
+    }
+
+    public static <R,S>  AB<R,S> a( Val<R> a , String msg ) {
+        return new AB<>(true , new NString(msg) , a , Val.empty() );
+    }
+
+
+    public static <R,S>  AB<R,S> b( Val<R> a ) {
+        return new AB<>(false , NString.NULL , a , Val.empty() );
+    }
+
+    public static <R,S>  AB<R,S> b( Val<R> a , String msg ) {
+        return new AB<>(false , new NString(msg) , a , Val.empty() );
+    }
+
+
+    @Override
+    public String toString() {
+        return "AB{" +
+                "ok=" + ok +
+                ", msg=" + msg +
+                ", a=" + a +
+                ", b=" + b +
+                '}';
+    }
+}
