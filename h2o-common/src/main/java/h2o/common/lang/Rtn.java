6e10a13860ab833a5a15605ae45f60b458838c76
zhangjw <zhangjw626@qq.com>
2022-11-26 22:12:29 +0800

    Rtn

--- h2o-common/src/main/java/h2o/common/lang/Rtn.java
+++ h2o-common/src/main/java/h2o/common/lang/Rtn.java
@@ -5,11 +5,20 @@ public final class Rtn<T> implements java.io.Serializable {
     private static final long serialVersionUID = 5164042784712875067L;
 
     public final boolean ok;
+    public final NString code;
     public final NString msg;
     public final Val<T> value;
 
     public Rtn(boolean ok, NString msg, Val<T> value) {
         this.ok = ok;
+        this.code = NString.NULL;
+        this.msg = msg;
+        this.value = value;
+    }
+
+    public Rtn(boolean ok, NString code, NString msg, Val<T> value) {
+        this.ok = ok;
+        this.code = code;
         this.msg = msg;
         this.value = value;
     }
@@ -26,28 +35,22 @@ public final class Rtn<T> implements java.io.Serializable {
         return new Rtn<>(true, NString.NULL, new Val<>(value));
     }
 
-    public static <R> Rtn<R> ok(String msg, R value) {
-        return new Rtn<>(true, new NString(msg), new Val<>(value));
-    }
-
     public static <R> Rtn<R> err(String msg) {
         return new Rtn<>(false, new NString(msg), Val.empty());
     }
 
-    public static <R> Rtn<R> err(String msg, R value) {
-        return new Rtn<>(false, new NString(msg), new Val<>(value));
+    public static <R> Rtn<R> err(String code , String msg) {
+        return new Rtn<>(false, new NString(code), new NString(msg), Val.empty());
     }
-
 
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("Rtn{");
-        sb.append("ok=").append(ok);
-        sb.append(", msg=").append(msg);
-        sb.append(", value=").append(value);
-        sb.append('}');
-        return sb.toString();
+        return "Rtn{" +
+                "ok=" + ok +
+                ", code=" + code +
+                ", msg=" + msg +
+                ", value=" + value +
+                '}';
     }
-
 }
