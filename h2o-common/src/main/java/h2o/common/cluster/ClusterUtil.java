b5a51876b304a10cb6193a8ec66fa95d24fa956c
zhangjw <zhangjw626@qq.com>
2023-04-27 20:21:10 +0800

    优化

--- h2o-common/src/main/java/h2o/common/cluster/ClusterUtil.java
+++ h2o-common/src/main/java/h2o/common/cluster/ClusterUtil.java
@@ -4,6 +4,7 @@ package h2o.common.cluster;
 import h2o.apache.commons.lang.StringUtils;
 import h2o.common.lang.SDate;
 import h2o.common.util.id.SnowGarlandIdGen;
+import h2o.common.util.lang.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,8 +41,23 @@ public abstract class ClusterUtil {
         public synchronized String makeId() {
             LocalDate localDate = LocalDate.now(ZoneOffset.UTC);
             String yyyy = Integer.toString(localDate.getYear());
-            return StringUtils.substring(yyyy, -2) +
-                    idGen.nextKey(new SDate(yyyy + "-01-01", true));
+            return StringUtil.build(StringUtils.substring(yyyy, -2) ,
+                    idGen.nextKey(new SDate(yyyy + "-01-01", true)));
+        }
+
+
+
+
+        private final static char[] LATIN = new char[] {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+        public synchronized String make20Id() {
+            LocalDate localDate = LocalDate.now(ZoneOffset.UTC);
+            String yyyy = Integer.toString(localDate.getYear());
+            return StringUtil.build(LATIN[ Integer.parseInt(StringUtils.substring(yyyy, -2)) % LATIN.length ] ,
+                    idGen.nextKey(new SDate(yyyy + "-01-01", true)));
         }
 
     }
