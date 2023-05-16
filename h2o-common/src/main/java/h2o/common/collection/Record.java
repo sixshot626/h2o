03469e07d9dbd961282f8a227a1e888fe27f59f4
zhangjw <zhangjw626@qq.com>
2023-05-16 14:27:34 +0800

    优化

---
+++ h2o-common/src/main/java/h2o/common/collection/Record.java
@@ -0,0 +1,29 @@
+package h2o.common.collection;
+
+import java.io.Serializable;
+import java.util.Map;
+
+public class Record<E> implements Serializable {
+
+    private static final long serialVersionUID = -7864995458948805128L;
+
+    private final Map<Object,Object> data;
+
+    public Record(Map<?, ?> data) {
+        this.data = (Map<Object,Object>)data;
+    }
+
+    public <R> R get( E key ) {
+        return (R) data.get( key );
+    }
+
+    public Map<Object,Object> data() {
+        return this.data;
+    }
+
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
+}
