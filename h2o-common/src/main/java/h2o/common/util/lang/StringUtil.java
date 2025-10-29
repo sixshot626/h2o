48e9fe6d5fbfb44d7db43a8140dac52f0539a906
zhangjw <zhangjw626@qq.com>
2025-10-29 15:57:44 +0800

    6.3.0

--- h2o-common/src/main/java/h2o/common/util/lang/StringUtil.java
+++ h2o-common/src/main/java/h2o/common/util/lang/StringUtil.java
@@ -7,6 +7,7 @@ import h2o.common.util.format.FormattingTuple;
 import h2o.common.util.format.MessageFormatter;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 public abstract class StringUtil {
@@ -86,6 +87,112 @@ public abstract class StringUtil {
             return sb;
         }
     }
+
+
+
+
+
+
+    public static long similarity( String str1 , String str2 ) {
+
+        Map<CharSequence, Integer> map = Arrays.stream(str1.split(""))
+                .collect(Collectors.toMap(c -> c, c -> 1, Integer::sum));
+        Map<CharSequence, Integer> map2 = Arrays.stream(str2.split(""))
+                .collect(Collectors.toMap(c -> c, c -> 1, Integer::sum));
+
+
+        double cos =  CosineSimilarity.INSTANCE.cosineSimilarity(map, map2);
+
+        System.out.println( cos );
+
+        return (long)((1 - cos) * 10000000000000000L);
+
+    }
+
+
+
+    private static final class CosineSimilarity {
+
+        /**
+         * Singleton instance.
+         */
+        static final CosineSimilarity INSTANCE = new CosineSimilarity();
+
+        /**
+         * Construct a new instance.
+         */
+        public CosineSimilarity() {
+            // empty
+        }
+
+        /**
+         * Calculates the cosine similarity for two given vectors.
+         *
+         * @param leftVector left vector
+         * @param rightVector right vector
+         * @return cosine similarity between the two vectors
+         */
+        public Double cosineSimilarity(final Map<CharSequence, Integer> leftVector,
+                                       final Map<CharSequence, Integer> rightVector) {
+            if (leftVector == null || rightVector == null) {
+                throw new IllegalArgumentException("Vectors must not be null");
+            }
+
+            final Set<CharSequence> intersection = getIntersection(leftVector, rightVector);
+
+            final double dotProduct = dot(leftVector, rightVector, intersection);
+            double d1 = 0.0d;
+            for (final Integer value : leftVector.values()) {
+                d1 += Math.pow(value, 2);
+            }
+            double d2 = 0.0d;
+            for (final Integer value : rightVector.values()) {
+                d2 += Math.pow(value, 2);
+            }
+            final double cosineSimilarity;
+            if (d1 <= 0.0 || d2 <= 0.0) {
+                cosineSimilarity = 0.0;
+            } else {
+                cosineSimilarity = dotProduct / (Math.sqrt(d1) * Math.sqrt(d2));
+            }
+            return cosineSimilarity;
+        }
+
+        /**
+         * Computes the dot product of two vectors. It ignores remaining elements. It means
+         * that if a vector is longer than other, then a smaller part of it will be used to compute
+         * the dot product.
+         *
+         * @param leftVector left vector
+         * @param rightVector right vector
+         * @param intersection common elements
+         * @return The dot product
+         */
+        private double dot(final Map<CharSequence, Integer> leftVector, final Map<CharSequence, Integer> rightVector,
+                           final Set<CharSequence> intersection) {
+            long dotProduct = 0;
+            for (final CharSequence key : intersection) {
+                dotProduct += leftVector.get(key) * (long) rightVector.get(key);
+            }
+            return dotProduct;
+        }
+
+        /**
+         * Returns a set with keys common to the two given maps.
+         *
+         * @param leftVector left vector map
+         * @param rightVector right vector map
+         * @return common strings
+         */
+        private Set<CharSequence> getIntersection(final Map<CharSequence, Integer> leftVector,
+                                                  final Map<CharSequence, Integer> rightVector) {
+            final Set<CharSequence> intersection = new HashSet<>(leftVector.keySet());
+            intersection.retainAll(rightVector.keySet());
+            return intersection;
+        }
+
+    }
+
 
 
     //---------------------------------------------------------------------
